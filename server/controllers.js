50a3641256ee90b93b1893a403ca14ecc9a587ff
Jayvee Aspa <jayveeaspa@Jayvees-MacBook-Pro.local>
2019-06-15 09:40:07 -0700

    add docstrings to controller methods

--- server/controllers.js
+++ server/controllers.js
@@ -1,23 +1,43 @@
-const Controller = {}
+const controller = {}
 
-Controller.getAllItems = (req, res, next) => {
+/**
+* getAllItems - returns all items from catalog
+
+*  @param next - Callback Function w signature (err, users)
+*/
+Controller.getAllItems = (next) => {
 
 }
 
+/**
+* updateItems - updates items on the database based on purchases
+
+* @param req - http.IncomingRequest
+* @param res - http.ServerResponse
+*/
 Controller.updateItems = (req, res, next) => {
 
 }
 
-Controller.updateItems = (req, res, next) => {
 
-}
+/**
+* createNewOrderDetail - creates an order detail for each shoe purchased
 
+* @param req - http.IncomingRequest
+* @param res - http.ServerResponse
+*/
 Controller.createNewOrderDetail = (req, res, next) => {
 
 }
 
+/**
+* createNewOrder - creates an aggregated collection of the orders placed
+
+* @param req - http.IncomingRequest
+* @param res - http.ServerResponse
+*/
 Controller.createNewOrder = (req, res, next) => {
 
 }
 
-module.exports = Controller
+module.exports = controller
