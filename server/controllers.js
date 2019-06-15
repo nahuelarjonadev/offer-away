b2e668bc50ac08988d04f623d51ddb9f568a0027
Jayvee Aspa <jayveeaspa@Jayvees-MacBook-Pro.local>
2019-06-15 12:22:30 -0700

    add dummy data & save to res.locals

--- server/controllers.js
+++ server/controllers.js
@@ -1,12 +1,21 @@
 const controllers = {}
 
+//DUMMY DATA - this isnt the right data format!! Just using it to send back to client :)
+const dummyData = {
+  'shoe1': '1',
+  'shoe2': '2',
+  'shoe3': '3',
+}
+
 /**
 * getAllItems - returns all items from catalog
-
-*  @param next - Callback Function w signature (err, users)
+* @param req - http.IncomingRequest
+* @param res - http.ServerResponse
+* @param next - Callback Function w signature (err, users)
 */
-controllers.getAllItems = (next) => {
-
+controllers.getAllItems = (req, res, next) => {
+  res.locals.dummyData = dummyData
+  next()
 }
 
 /**
