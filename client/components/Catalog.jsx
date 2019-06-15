4d81bb7857d0133a2709242f85d69ff5dc165df5
gp5yk <nahuel.arjona.93@gmail.com>
2019-06-15 15:57:31 -0700

    refactor catalog to be a class component

--- client/components/Catalog.jsx
+++ client/components/Catalog.jsx
@@ -1,20 +1,37 @@
-import React, { useState } from 'react';
+import React, {Component} from 'react'
 
-function Catalog() {
-  const [items, setItems] = useState([]);
+class Catalog extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [<div key={1}>shoe1</div>, <div>shoe2</div>,<div>shoe3</div>]
+    }
+  }
 
-  fetch('/api/products')
-    .then(res => res.json())
-    .then(resItems => {
-      const itemsArr = Array.isArray(resItems) ? resItems : Object.keys(resItems);
-      setItems(itemsArr);
-    })
-  
-  return (
-    <div id='catalog'>
-      {items}
-    </div>
-  );
-}
+  render() {
+    return (
+      <div id='catalog'>
+        {this.state.items}
+      </div>
+    )
+  }
+} 
 
 export default Catalog;
+
+// function Catalog() {
+//   const [items, setItems] = useState([]);
+
+  // fetch('/api/products')
+  //   .then(res => res.json())
+  //   .then(resItems => {
+  //     const itemsArr = Array.isArray(resItems) ? resItems : Object.keys(resItems);
+  //     setItems(itemsArr);
+  //   })
+  
+//   return (
+//     <div id='catalog'>
+//       {items}
+//     </div>
+//   );
+// }
