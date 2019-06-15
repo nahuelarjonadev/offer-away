5c5aaf58104e9f055b81fa2c51c789e63458a003
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-06-15 11:52:34 -0700

    display Navbar in Header

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import CartBtn from './CartBtn';
+import NavBar from './Navbar';
 
 function Header() {
   return (
     <header>
+      <NavBar />
       <CartBtn />
     </header>
   );
