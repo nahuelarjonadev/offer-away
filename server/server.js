3e560aac664c25657ebb1de975f7070c8ec0fca0
Jayvee Aspa <jayveeaspa@Jayvees-MacBook-Pro.local>
2019-06-14 23:37:46 -0700

    comment needed middleware methods for /catalog & /purchase routes

--- server/server.js
+++ server/server.js
@@ -12,12 +12,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //return list of all the in stock items
 app.get('/catalog', (req, res) => {
-
+  //get all items from database
 })
 
 //purchase route
 app.post('/purchase', (req, res) => {
-
+  //update items in database
+  //create new customer if not already in database
+  //create new order for the purchase
 })
 
 app.listen(PORT, () => {
