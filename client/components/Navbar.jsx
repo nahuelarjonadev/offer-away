49f11b8cf44dab05e24aa2545610c46c32c1be43
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-06-15 11:52:17 -0700

    add boilerplate to NavBar

---
+++ client/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NavBar() {
+  return (
+    <ul>
+      <li><a href="Home">Home</a></li>
+      <li><a href="Purchase">Purchase</a></li>
+    </ul>
+  );
+}
+
+export default NavBar;
