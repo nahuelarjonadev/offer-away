22f818b5b082a6b83668438ec989ec0717158f36
gp5yk <nahuel.arjona.93@gmail.com>
2019-06-15 11:47:24 -0700

    add boilerplate to CartBtn

---
+++ client/components/CartBtn.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CartBtn() {
+  return (
+    <button>
+      <img src="/static/icon/cart" alt="Cart Img"></img>
+    </button>
+  );
+}
+
+export default CartBtn;
