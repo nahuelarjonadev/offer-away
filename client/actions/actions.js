ae835ce1c8a448d3f44656edb4eea6e20d3b3474
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-06-15 20:03:13 -0700

    fix bug in error handling on fetchProducts

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -9,15 +9,27 @@ export const receiveProducts = (json) => ({
   payload: json,
 })
 
+/**
+ * Dispatch this in case of receiving invalid data or the request fails
+ * @param {*} err error object
+ */
 export const requestProductsFailure = (err) => ({
-  type: types.RECEIVE_PRODUCTS,
+  type: types.REQUEST_PRODUCTS_FAILURE,
   payload: err,
 })
 
 export const fetchProducts = () => dispatch => {
-  dispatch(requestProducts);
-  return fetch('/api/products')
+  console.log('fetchProducts');
+  dispatch(requestProducts());
+  return fetch('/api/productsa')
     .then(res => res.json())
-    .then(res => dispatch(receiveProducts(res)))
+    .then(res => {
+      if (!isValidProducts(res)) throw new Error('something went wrong')
+      return dispatch(receiveProducts(res))
+    })
     .catch(err => dispatch(requestProductsFailure(err)))
 }
+function isValidProducts(res) {
+  return Array.isArray(res);
+}
+
