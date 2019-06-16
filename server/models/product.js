96886693bcf07f37f2a14aa5cb6166aa05ac1cd7
Alex Yen <alexyen@Alexs-MacBook-Pro.local>
2019-06-15 20:33:48 -0700

    add category route

--- server/models/product.js
+++ server/models/product.js
@@ -14,6 +14,9 @@ const pool = new Pool({
 const GET_ALL = `SELECT "Product"."product_name", "size", "inventory", "price", "Category"."category_name" from 
 "Product" join "Category" on "Product"."category_id"="Category"."category_id";`;
 
+const GET_CATEGORY = `SELECT "Product"."product_name", "size", "inventory", "price", "Category"."category_name" from
+"Product" join "Category" on "Product"."category_id"="Category"."category_id" WHERE "Category"."category_name"=`;
+
 const productModel =  {
   getAll() {
     return new Promise((resolve, reject) => {
@@ -21,6 +24,14 @@ const productModel =  {
         if (err) return reject(err);
         resolve(result);
       });
+    })
+  },
+  getCategory(categoryName) {
+    return new Promise((resolve, reject) => {
+      pool.query(GET_CATEGORY + `'${categoryName}'` + ";", (err, result) => {
+        if (err) return reject(err);
+        resolve(result);
+      })
     })
   }
 };
