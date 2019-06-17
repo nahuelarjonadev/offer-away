bd825eb50d31b81d1c5ad94ae4689ccab66882ff
gp5yk <nahuel.arjona.93@gmail.com>
2019-06-17 15:16:28 -0700

    complete checkoutModal with basic styling

--- client/components/CartBtn.jsx
+++ client/components/CartBtn.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function CartBtn(props) {
   return (
-    <button id='cart-btn'>
+    <button id='cart-btn' onClick={props.proceedToCheckout}>
       <p>{props.totalItemsInCart}</p>
       <img src="/static/cart-logo3.png" width="50px" alt="Cart Img"></img>
     </button>
--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -3,23 +3,28 @@ import CartBtn from './CartBtn';
 import NavBar from './Navbar';
 import { connect } from "react-redux";
 
+import * as actions from '../actions/actions';
+
 const mapStateToProps = store => ({
   totalItemsInCart: store.products.totalItemsInCart,
+})
+
+const mapDispatchToProps = dispatch => ({
+  proceedToCheckout: () => dispatch(actions.proceedToCheckout()),
 })
 
 function Header(props) {
   return (
     <header>
       <div className='header-left'>
-        
         <h1>SNiX</h1>
       </div>
       <div className='header-right'>
         <NavBar />
-        <CartBtn totalItemsInCart={props.totalItemsInCart} />
+        <CartBtn proceedToCheckout={props.proceedToCheckout} totalItemsInCart={props.totalItemsInCart} />
       </div>
     </header>
   );
 }
 
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
