432553b12916c8741547f9bfefaf483f4f212896
gp5yk <nahuel.arjona.93@gmail.com>
2019-06-17 15:27:33 -0700

    add header and total price to checkout modal

--- client/components/PurcasheModal.jsx
+++ client/components/PurcasheModal.jsx
@@ -13,14 +13,16 @@ const mapDispatchToProps = dispatch => ({
 })
 
 function PruchaseModal(props) {
+  let purchaseTotalPrice = 0;
   const products = Object.entries(props.cart).map(([SKU, quantity]) => {
     const product = Object.values(props.products).filter(p => p.SKU == SKU)[0];
-    const finalPrice = parseInt(product.price) * parseInt(quantity)
+    const productTotalPrice = parseInt(product.price) * parseInt(quantity);
+    purchaseTotalPrice += productTotalPrice;
     return (
       <li className="productSummary">
         <span> {quantity}</span>
         <span> {product.product_name} </span>
-        <span> ${finalPrice} </span>
+        <span> ${productTotalPrice} </span>
       </li>
     )
   });
@@ -31,8 +33,9 @@ function PruchaseModal(props) {
           <button onClick={props.exitCheckout}>Exit</button>
           <div id="checkoutSummary">
             <ul>
-              <li><span>Quantity</span><span>Description</span><span>Price</span></li>
+              <li className="purchaseHeader"><span>Quantity</span><span>Description</span><span>Price</span></li>
               {products}
+              <li className="purchaseHeader"><span>Total:</span><span></span><span> $ {purchaseTotalPrice}</span></li>
             </ul>
           </div>
         </div>
