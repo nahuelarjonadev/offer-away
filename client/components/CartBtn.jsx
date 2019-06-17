c548cdd3892798482d80d975bfc8feaa6225bcd8
arshiamasih <arshia.masih@gmail.com>
2019-06-17 12:11:39 -0700

    items view component

--- client/components/CartBtn.jsx
+++ client/components/CartBtn.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function CartBtn() {
   return (
     <button id='cart-btn'>
-      <p>5</p>
+      <p>#items</p>
       <img src="/static/icon/cart" alt="Cart Img"></img>
     </button>
   );
