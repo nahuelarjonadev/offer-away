3f75eebde290bc2e252b74c0044278d0ca385e8c
gp5yk <nahuel.arjona.93@gmail.com>
2019-06-18 14:54:03 -0700

    make product.js require and use pool.js

--- server/models/product.js
+++ server/models/product.js
@@ -1,15 +1,4 @@
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  user: "xqtwqblw",
-  host: "raja.db.elephantsql.com",
-  database: "xqtwqblw",
-  password: "HOn-ooZWg4SonxCxotW_5cBofitQIVAX",
-  port: 5432,
-  max: 20,
-  idleTimeoutMillis: 30000,
-  _connectionTimeoutMillis: 2000,
-});
+const pool = require('../pool');
 
 const GET_ALL = `SELECT "Product"."SKU", "Product"."product_name", "size", "inventory", "price", "Category"."category_name" from 
 "Product" join "Category" on "Product"."category_id"="Category"."category_id";`;
