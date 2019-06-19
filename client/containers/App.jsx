261eaf74e83f41aff32999e6bf0b6fd2f65d991c
gp5yk <nahuel.arjona.93@gmail.com>
2019-06-18 21:17:48 -0700

    redirect wrong routes to NotFound

--- client/containers/App.jsx
+++ client/containers/App.jsx
@@ -5,6 +5,8 @@ import Purchase from '../containers/Purchase';
 import Home from '../containers/Home';
 import Header from '../containers/Header';
 import Footer from '../components/Footer';
+import NotFound from '../components/NotFound';
+
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <Header />
       <Route path="/" exact component={Home} />
       <Route path="/purchase" component={Purchase} />
+      <Route component={NotFound} />
       <Footer />
     </Router>
   );
