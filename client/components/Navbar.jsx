b5cac2028662f2bf78fc662cb2f52a8185ef4ccf
gp5yk <nahuel.arjona.93@gmail.com>
2019-06-18 21:06:55 -0700

    header and footer are always displayed

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
     <ul className='navbar'>
-      <li><a href="Home">Home</a></li>
-      <li><a href="Purchase">Purchase</a></li>
+      <li><Link to='/'>Home</Link></li>
+      <li><Link to='/Purchase'>Purchase</Link></li>
     </ul>
   );
 }
