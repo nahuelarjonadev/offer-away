bb9b0a7b0800e3d814bd569421ae34c7a5a58d97
Brian Kwok <briankwok@Brians-MacBook-Pro.local>
2019-06-18 18:45:12 -0700

    delete complete

--- server/routes/businessapi.js
+++ server/routes/businessapi.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const productCtrl = require('../controllers/product-controller');
 
-router.post('/create-product', productCtrl.addProduct, (req, res) => {
-  res.status(200).json(res.locals.response);
-})
+// router.post('/create-product', productCtrl.addProduct, (req, res) => {
+//   res.status(200).json(res.locals.response);
+// })
 
-router.get('/delete', productCtrl.deleteProduct, (req, res) => {
+router.delete('/delete', productCtrl.deleteProduct, (req, res) => {
   res.status(200).send(res.locals.deleted)
 })
 
