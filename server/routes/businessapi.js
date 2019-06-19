2a2354c74b09aaa14217496cc15522fab62ff5a3
Brian Kwok <briankwok@Brians-MacBook-Pro.local>
2019-06-19 12:59:36 -0700

    upload image route complete

--- server/routes/businessapi.js
+++ server/routes/businessapi.js
@@ -2,6 +2,18 @@ const express = require('express');
 const router = express.Router();
 const productCtrl = require('../controllers/product-controller');
 const contactCtrl = require('../controllers/contactInfo-controller');
+const multer = require('multer');
+// configuration on how files get stored
+// we will pass this function when a file is uploaded
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null,'./uploads/') // <- where to store
+  },
+  filename: function(req, file, cb){
+    cb(null, new Date().toISOString() + file.originalname) // <- set new name for uploaded image
+  }
+})
+const upload = multer({storage: storage}); // <- implement 
 
 // created a delete route
 // will return an object {success: true, result: result} back to the client
@@ -27,6 +39,12 @@ router.get('/categories', categoryCtrl.getCategories, (req, res) => {
 
 router.post('/categories', categoryCtrl.createCategory, (req, res) => {
   res.status(200).json(res.locals.response);
+})
+
+// create route post route for image upload
+// multer, just like body parser actually parses form data
+router.post('/uploadImage', upload.single('productImage'), (req, res) => {
+  res.status(200).json({message: 'completed'})
 })
 
 module.exports = router;
