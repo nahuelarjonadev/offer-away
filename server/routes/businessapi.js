97201a1676730a0df01c04ba47a445ce3c7e677d
Joseph Eisele <eisele.joseph1@gmail.com>
2019-06-18 19:32:11 -0700

    created GET /categories in businessapi.js

--- server/routes/businessapi.js
+++ server/routes/businessapi.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const productCtrl = require('../controllers/product-controller');
+const categoryCtrl = require('../controllers/category-controller');
 
 router.post('/create-product', productCtrl.addProduct, (req, res) => {
+  res.status(200).json(res.locals.response);
+})
+
+router.get('/categories', categoryCtrl.getCategories, (req, res) => {
   res.status(200).json(res.locals.response);
 })
 
