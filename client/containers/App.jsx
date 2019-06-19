e4b99f58bdf7dd393122c511bcc5afd213597104
gp5yk <nahuel.arjona.93@gmail.com>
2019-06-18 17:52:08 -0700

    use react router in App

--- client/containers/App.jsx
+++ client/containers/App.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
-import Header from './Header';
-import MainDisplay from '../components/MainDisplay'
-import Footer from '../components/Footer'
-import PurchaseModal from './PurcasheModal';
-import { connect } from "react-redux";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-const mapStateToProps = store => ({
-  onCheckoutPage: store.products.onCheckoutPage,
-})
+import PurchaseModal from '../containers/PurcasheModal';
+import Home from '../containers/Home'
 
-function App({ onCheckoutPage }) {
+function App() {
   return (
-    <div>
-      <Header />
-      <MainDisplay />
-      <Footer />
-      {onCheckoutPage && <PurchaseModal />}
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/img">Img 1</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Home} />
+        <Route path="/img" component={PurchaseModal} />
+      </div>
+    </Router>
   );
 }
 
-export default connect(mapStateToProps)(App);
+export default App;
