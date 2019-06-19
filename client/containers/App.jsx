b54d60177e615a5201068959858c5e591b480357
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-06-18 21:06:55 -0700

    header and footer are always displayed

--- client/containers/App.jsx
+++ client/containers/App.jsx
@@ -2,26 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import Purchase from '../containers/Purchase';
-import Home from '../containers/Home'
+import Home from '../containers/Home';
+import Header from '../containers/Header';
+import Footer from '../components/Footer';
 
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/purchase">Purchase</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Route path="/" exact component={Home} />
-        <Route path="/purchase" component={Purchase} />
-      </div>
+      <Header />
+      <Route path="/" exact component={Home} />
+      <Route path="/purchase" component={Purchase} />
+      <Footer />
     </Router>
   );
 }
