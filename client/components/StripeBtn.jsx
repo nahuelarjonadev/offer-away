c7a380f02ee02ae98bf82a21a32f3aa0cadb8344
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-06-19 18:07:36 -0700

    create StripeBtn component

---
+++ client/components/StripeBtn.jsx
@@ -0,0 +1,49 @@
+import React, { Fragment } from "react";
+import StripeCheckout from "react-stripe-checkout";
+const stripeBtn = () => {
+  const publishableKey = "pk_test_ZU3mlTy0q00DATc9EyF9A8jX";
+
+  const onToken = token => {
+    const body = {
+      amount: 999,
+      token: token
+    };
+
+    fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, cors, *same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrer: 'no-referrer', // no-referrer, *client
+      body: JSON.stringify(body), // body data type must match "Content-Type" header
+    })
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+        alert("Payment Success");
+      })
+      .catch(error => {
+        console.log("Payment Error: ", error);
+        alert("Payment Error");
+      });
+  };
+  
+  return (
+    <StripeCheckout
+      label="Buy Item" //Component button text
+      name="Fire Frost LLC" //Modal Header
+      description="Buy your sneakers today!"
+      panelLabel="Buy Item" //Submit button in modal
+      amount={999} //Amount in cents $9.99
+      token={onToken}
+      stripeKey={publishableKey}
+      image="https://www.vidhub.co" //Pop-in header image
+      billingAddress={false}
+    />
+  );
+};
+export default stripeBtn;
