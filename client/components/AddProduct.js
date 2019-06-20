2654da521610888c7c9afb2b648a6b4e9218d3a0
Joseph Eisele <eisele.joseph1@gmail.com>
2019-06-19 22:41:49 -0700

    dummy add product form test

--- client/components/AddProduct.js
+++ client/components/AddProduct.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import { Form, Field } from 'react-final-form';
 
-function AddProduct(props) {
+const onSubmit = (values) => {
+  alert(JSON.stringify(values));
+};
+
+function AddProduct() {
   return(
     <div className="overlay">
       <div className="modal">
         <h2>Add a Product</h2>
-        
+        <Form
+          onSubmit={onSubmit}
+          render={({ handleSubmit }) => (
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label>Product Name</label>
+                <Field name="productName" component="input" type="text" />
+              </div>
+              <div>
+                <label>Category</label>
+                <Field name="category" component="input" type="text" />
+              </div>
+              <div>
+                <label>Size</label>
+                <Field name="size" component="input" type="text" />
+              </div>
+              <div>
+                <label>Available Inventory</label>
+                <Field name="inventory" component="input" type="text" />
+              </div>
+              <div>
+                <label>Price</label>
+                <Field name="price" component="input" type="text" />
+              </div>
+              <button type="submit">Submit</button>
+            </form>
+          )}
+        />
       </div>
     </div>
   )
