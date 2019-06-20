354094a3e8246f4a7dc9f4aa8598af257635ec76
gp5yk <nahuel.arjona.93@gmail.com>
2019-06-19 19:02:46 -0700

    render StripeBtn in Checkout

--- client/components/Checkout.jsx
+++ client/components/Checkout.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import StripeBtn from '../components/StripeBtn';
 
 function Checkout({location: {state: cart}}) {
   return (
     <div>
       {JSON.stringify(cart)}
+      <StripeBtn />
     </div>
   );
 }
