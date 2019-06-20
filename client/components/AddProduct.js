ba1c0e0a65bbdfea0f8b60abfb694b262af3692a
Joseph Eisele <eisele.joseph1@gmail.com>
2019-06-19 21:41:47 -0700

    installed react-final-form, created popup skeleton for add product form

---
+++ client/components/AddProduct.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function AddProduct(props) {
+  return(
+    <div className="overlay">
+      <div className="modal">
+        <h2>Add a Product</h2>
+        
+      </div>
+    </div>
+  )
+}
+
+export default AddProduct;
