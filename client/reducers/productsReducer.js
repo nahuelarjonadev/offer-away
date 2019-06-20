9c764cbf3b26df0486195dd9a8e55683a4be3b0d
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-06-20 12:52:00 -0700

    complete addProduct actions and functionality

--- client/reducers/productsReducer.js
+++ client/reducers/productsReducer.js
@@ -1,8 +1,9 @@
-import { REQUEST_PRODUCTS, RECEIVE_PRODUCTS, REQUEST_PRODUCTS_FAILURE, PROCEED_TO_CHECKOUT, EXIT_CHECKOUT, SET_BUTTON_TEXT, RESET_BUTTON_TEXT, GOTO_ADD_PRODUCT, EXIT_ADD_PRODUCT } from '../constants/actionTypes';
+import { REQUEST_PRODUCTS, RECEIVE_PRODUCTS, REQUEST_PRODUCTS_FAILURE, PROCEED_TO_CHECKOUT, EXIT_CHECKOUT, SET_BUTTON_TEXT, RESET_BUTTON_TEXT, GOTO_ADD_PRODUCT, EXIT_ADD_PRODUCT, POST_ADD_PRODUCT, ADD_PRODUCT_SUCCESS, ADD_PRODUCT_FAILURE } from '../constants/actionTypes';
 
 const initialState = {
   products: [{SKU: 1, product_name: 'product description', size: 0, inventory: '0', price: '0', category_name: 'category'}],
   fetchProductsStatus: '',
+  postProductStatus: '',
   fetchProductsError: '',
   currentCategory: '',
   onCheckoutPage: false,
@@ -11,21 +12,21 @@ const initialState = {
   buttonTwoText: 'Delete From Cart',
 }
 
-const productsReducer = (state = initialState, action) => {
-  switch (action.type) {
+const productsReducer = (state = initialState, {type, payload}) => {
+  switch (type) {
     case REQUEST_PRODUCTS:
       return { ...state, fetchProductsStatus: 'pending' };
     case RECEIVE_PRODUCTS:
       return {
         ...state,
         fetchProductsStatus: 'success',
-        products: action.payload
+        products: payload
       }
     case REQUEST_PRODUCTS_FAILURE:
       return {
         ...state,
         fetchProductsStatus: 'failure',
-        fetchProductsError: action.payload,
+        fetchProductsError: payload,
       }
     case PROCEED_TO_CHECKOUT:
       return {
@@ -59,6 +60,21 @@ const productsReducer = (state = initialState, action) => {
       return {
         ...state,
         onAddProductPage: false,
+      }
+    case POST_ADD_PRODUCT:
+      return {
+        ...state,
+        postProductStatus: 'Creating...'
+      }
+    case ADD_PRODUCT_SUCCESS:
+      return {
+        ...state,
+        postProductStatus: 'Successfully created!'
+      }
+    case ADD_PRODUCT_FAILURE:
+      return {
+        ...state,
+        postProductStatus: payload,
       }
     default:
       return state;
